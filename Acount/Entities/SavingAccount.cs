4d300a06d80ad912c3c52a06ad92282f5e65bf37
João Almeida <joaoalmeida48@outlook.com>
2022-12-10 21:47:39 -0300

    primeiro Commit

---
+++ Acount/Entities/SavingAccount.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Acount.Entities
+{
+    class SavingAccount : Account
+    {
+        public double InteresRate { get; set; }
+
+
+        public SavingAccount()
+        {
+
+        }
+
+        public SavingAccount(int number, string holder, double balance, double interesRate) 
+            :base(number,holder,balance)
+        {
+            InteresRate = interesRate;
+        }
+
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InteresRate;
+        }
+
+        public override void WithDraw(double amount)
+        {
+            base.WithDraw(amount);
+
+            Balance -= 2.0;
+        }
+    }
+}
---
+++ Acount/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Acount.Entities;
+
+namespace Acount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Account acc1 = new Account(1001, "alex", 500.0);
+            Account acc2 = new Account(1002, "Anna", 500.0);
+
+            acc1.WithDraw(10);
+            acc2.WithDraw(20);
+            
+
+            Account acc3 = new SavingAccount(1002, "Anna", 500.0, 0.01);
+
+            acc3.WithDraw(10);
+
+            Console.WriteLine(acc1.Balance);
+            Console.WriteLine(acc3.Balance);
+            
+        }
+    }
+}
